0fdb72af7752d76090dcc83d3dd03c24e1872043
Anouchka MINKOUE OBAME <informaticaux@gmail.com>
2020-08-07 19:55:22 +0200

    Initial commit

---
+++ commentaires.php
@@ -0,0 +1,10 @@
+<?php 
+
+//Ceci est un commentaire court sur une ligne
+
+/* Ceci est commentaire abondant
+qui va occuper plusieurs lignes
+et va expliquer le code qui suit............. */
+
+
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Programmation PHP</title>
+<link href="styles.css" rel="stylesheet">
+</head>
+<body>
+
+<header>
+<nav>
+    <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Qui sommes-nous</a></li>
+        <li><a href="#">Nos services</a></li>
+        <li><a href="#">Nous contacter</a></li>
+    </ul>
+
+</nav>
+</header>
+<main>
+Ceci est une page HTML utilisant PHP
+</main>
+
+<footer>
+    Pied du site - Créé en 2020
+</footer>
+
+</body>
+</html>
